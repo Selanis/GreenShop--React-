861da69400bdeac129e90ef8b0e59ffb17052635
Selanis <k.polexowa@yandex.ru>
2024-03-11 14:04:09 +0300

    Вывела карточки на экран

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -4,22 +4,78 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 
 class Data extends Component {
+    state = {
+        apiKey: '46086b20-ac10a08c-d2bdd200-b171b52e'
+    }
+
+    handleClickBuy = (data) => {
+        localStorage.setItem(data.mainId, 1)
+    }
+
+    componentDidMount = async () => {
+        $.ajax({
+            url: `https://fortniteapi.io/v2/shop?lang=ru`,
+            method: 'GET',
+            headers: {
+                accept: 'application/json', 
+                Authorization: '46086b20-ac10a08c-d2bdd200-b171b52e'
+                
+            },
+            
+            beforeSend: function(xhr){
+                let newContent = ``
+                newContent += `
+                    <div class="preloader">
+                            <img class="preloader__logo" id="preloader" src="preloader.svg" width="50px" height="50px" />
+                    </div>
+                `
+                $('.data').html(newContent)
+            },
+            success: function (data) {
+                console.log(data)
+                let newContent = ``
+                for (let i = 50; i < 80; i++) {
+                    if (localStorage.getItem(data.shop[i].mainId) == null) {
+                        newContent += `
+                            <div class='data__item'>
+                                <div class="image" style="background: url('${data.shop[i].granted[0].images.background}') center/cover no-repeat;"></div>
+
+                                <div class="text">
+                                    <p>${data.shop[i].displayName}</p>
+                                    <h3>${data.shop[i].price.finalPrice}₽</h3>
+                                    <button class="text__button" >Купить</button>
+                                </div>
+                            </div>
+                        `
+                    } else {
+                        newContent += `
+                            <div class='data__item'>
+                                <div class="image" style="background: url('${data.shop[i].granted[0].images.background}') center/cover no-repeat;"></div>
+
+                                <div class="text">
+                                    <p>${data.shop[i].displayName}</p>
+                                    <h3>${data.shop[i].price.finalPrice}₽</h3>
+                                    <div class="text__buttons">
+                                        <button>-</button>
+                                        <h3>${localStorage.getItem(data.shop[i].mainId != null)}</h3>
+                                        <button>+</button>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+                    }
+                }
+
+                $('.data').html(newContent)
+            }
+        })
+    }
+
     render() {
         return (
             <section>
                 <div className="data" style={{minHeight: 'calc(100vh - 120px - 78px - 18px)'}}>
-                    {/* <div className="data__preloader">
-                    <img src="preloader.svg" />
-                    </div> */}
 
-                    <div className='data__item'>
-                        <div className="image"></div>
-
-                        <div className="text">
-                            <p>Barberton Daisy</p>
-                            <h3>$119.00</h3>
-                        </div>
-                    </div>
                 </div>
             </section>
         )
