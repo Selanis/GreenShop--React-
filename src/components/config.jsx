6aa0b561425bfd6e5664462b278fb682fcae02ac
Selanis <k.polexowa@yandex.ru>
2024-03-28 09:36:52 +0300

    Переменную окружения пихнула

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,4 +1,4 @@
-const API_KEY = '46086b20-ac10a08c-d2bdd200-b171b52e'
+const API_KEY = process.env.REACT_APP_API_KEY
 const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru'
 
 export { API_KEY, API_URL }
